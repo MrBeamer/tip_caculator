8d8089b6b5d5717fbe9871a542194b2e3db44aea
michael beamer <beamermichael@gmail.com>
2020-12-25 19:24:51 +0100

    finished js caculator

---
+++ calculator.js
@@ -0,0 +1,49 @@
+function calculate () {
+let billingAmount = document.getElementById("billing-amount").value;
+let serviceQuality = document.getElementById("service-quality").value;
+let peopleAmount = document.getElementById("people-amount").value;
+
+console.log(billingAmount);
+console.log(serviceQuality);
+console.log(peopleAmount);
+
+// validate billing amount
+if (billingAmount === "" || serviceQuality  == 0) {
+alert("Ups, you forgot something.")
+return;
+}
+
+// checking if each should show or not
+if (peopleAmount === "" || peopleAmount <= 1) {
+  peopleAmount = 1;
+  document.getElementById("each").style.display = "none";
+} else {
+  document.getElementById("each").style.display = "block";
+}
+
+
+ //Calculate tip
+let tip = (billingAmount*serviceQuality)/peopleAmount;
+
+//round to two decimal places
+tip = Math.round(tip * 100) / 100
+
+//next line allows to always have two digits after decimal point
+tip = tip.toFixed(2);
+
+console.log(tip);
+
+// Display the tip (function called)
+document.getElementById("boxTip").style.display = "block";
+document.getElementById("tip").innerHTML = `${tip} Euro`;
+}
+
+
+// Hidden tip amount on load / initial display state none (before calling function)
+document.getElementById("boxTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+//click button -> call function and start to calculating the tip -> displaying the tip
+document.getElementById("calculate").onclick = function () {
+  calculate();
+};
